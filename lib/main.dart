4492bee730406a57305aa1ccfcca780582095fcc
Otavio Miguel <otavio@vidroid.com.br>
2019-03-10 17:26:20 -0300

    A

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,169 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Exploring UI Widgets",
-    home: Home(),
+    home: Scaffold(
+      appBar: AppBar(title: Text("Que app feio!"),),
+      body: getListView(),
+    ),
   ));
+}
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+        onTap: () {},
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+        onLongPress: () {},
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Ouvi voce"),
+        subtitle: Text("Juro que ouvi"),
+        trailing: Icon(Icons.subway),
+      ),
+
+    ],
+  );
+  return listView;
 }
