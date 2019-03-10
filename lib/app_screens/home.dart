13c9690573fb902887bcae5d28b1321ef9528ad4
Otavio Miguel <otavio@vidroid.com.br>
2019-03-10 16:39:21 -0300

    new

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Column(
+            children: <Widget>[
+              Text(
+                "La",
+                style: TextStyle(decoration: TextDecoration.none),
+                textDirection: TextDirection.ltr,
+              ),
+              Text(
+                "Flight",
+                style: TextStyle(decoration: TextDecoration.none),
+                textDirection: TextDirection.ltr,
+              ),
+              BuyTicketsButton()
+            ],
+          )),
+    );
+  }
+}
+
+class BuyTicketsButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+          color: Colors.deepOrange,
+          child: Text("Buy tickets"),
+          elevation: 6.0,
+          onPressed: () => buyTickets(context)),
+    );
+  }
+
+  void buyTickets(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("get tickets"),
+      content: Text("Tickets are already yours!"),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'package:flutter/material.dart';
-import './app_screens/first_screen.dart';
+import './app_screens/home.dart';
 
-void main() => runApp(MyFlutterApp());
-
-
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "My flutter app",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("My first app screen"),
-            backgroundColor: Colors.red,
-          ),
-          body: FirstScreen()
-        )
-    );
-  }
-
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Exploring UI Widgets",
+    home: Home(),
+  ));
 }
